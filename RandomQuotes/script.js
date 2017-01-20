e12b44d87b917097e4daff2747f3ebbdfc419380
Ioannis <yannoj2@gmail.com>
2017-01-20 12:13:22 -0500

    A new quote appears with every press of the button 'New Quote'

---
+++ RandomQuotes/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('#get-quote').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $('#quote-content').html(post.content);
+        $('#quote-author').text(post.title);
+      },
+      cache: false
+    });
+  });
+});
+
+function change() {
+	var x = Math.floor(Math.random() * 256); // range is 0-255
+	var y = Math.floor(Math.random() * 256);
+	var z = Math.floor(Math.random() * 256);
+	var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
+	elements = document.getElementsByClassName("bkgc");
+	for (var i=0; i<elements.length; i++) {
+		elements[i].style.backgroundColor=thergb;
+	}
+}
