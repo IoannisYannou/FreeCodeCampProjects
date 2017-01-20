a6f719f736957fd0f41b18bcb60a2fd5f008d205
Ioannis <yannoj2@gmail.com>
2017-01-20 12:16:30 -0500

    Altered url from 'http' to 'https' in order to resolve the 'Blocked loading active mixed content' issue

--- RandomQuotes/script.js
+++ RandomQuotes/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#get-quote').on('click', function(e) {
     e.preventDefault();
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
         $('#quote-content').html(post.content);
