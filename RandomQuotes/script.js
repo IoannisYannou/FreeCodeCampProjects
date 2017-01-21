8aeccb93f7101421f96d11bb788f09582037c607
Ioannis <yannoj2@gmail.com>
2017-01-20 22:50:18 -0500

    Font Awesome quote now works harmoniously with quotes

--- RandomQuotes/script.js
+++ RandomQuotes/script.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
       url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-content').html("<i class='fa fa-quote-left'></i>" + post.content);
+        var content = post.content;
+        $('#quote-content').html("<i class='fa fa-quote-left'></i>" + content.slice(3,-5));
         $('#quote-author').html("- " + post.title);
       },
       cache: false
